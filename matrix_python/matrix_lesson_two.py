060a40077724eb92f0700d8ab1fdb005c590db2d
Julian Radovanov <jradovanov@gmail.com>
2023-05-26 13:12:48 +0300

    Some uodates

--- matrix_python/matrix_lesson_two.py
+++ matrix_python/matrix_lesson_two.py
@@ -1,15 +1,11 @@
-# def multiply(*args):
-#     result = 1
-#     for nums in args:
-#         result *= nums
-#     return result
-#
-#
-# print(multiply(2, 4, 5))
+def multiply(*args):
+    result = 1
+    for nums in args:
+        result *= nums
+    return result
+
+
+print(multiply(2, 4, 5))
 def get_info(**kwargs):
     return kwargs
 
-
-print(get_info(f"This is {} from {} and he is {} years old")
-{"name": "George", "town":
-"Sofia", "age": 20
