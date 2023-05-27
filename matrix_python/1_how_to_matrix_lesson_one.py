7a7244bae470e8b27148def4e4f22ec112fddc52
Julian Radovanov <jradovanov@gmail.com>
2023-05-28 00:02:17 +0300

    Update 1_how_to_matrix_lesson_one.py

--- matrix_python/1_how_to_matrix_lesson_one.py
+++ matrix_python/1_how_to_matrix_lesson_one.py
@@ -33,3 +33,27 @@ print(matrix)
 # one line way to have a matrix
 matrix = [[int(x) for x in input().split()] for row in range(3)]
 print(matrix)
+
+# row = int(input())
+# matrix = []
+#
+# for _ in range(row):
+#     inner_list = [int(x) for x in input().split(", ")]
+#     matrix.append(inner_list)
+# print(matrix)
+# ===================================
+# row = int(input())
+# matrix = []
+# for _ in range(row):
+#     inner_list = []
+#     data = input().split(", ")
+#     for el in data:
+#         inner_list.append(int(el))
+#     matrix.append(inner_list)
+# print(matrix)
+# =================================
+# row = int(input())
+#
+# matrix = [[int(x) for x in input().split(", ")] for _ in range(row)]
+#
+# print(matrix)88
