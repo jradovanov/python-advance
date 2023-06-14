cb0cd93666cf381415ba0780a5c2736c5dc9043c
Julian Radovanov <jradovanov@gmail.com>
2023-06-15 00:00:16 +0300

    Update args_and_kwargs.py

--- functions_advanced/args_and_kwargs.py
+++ functions_advanced/args_and_kwargs.py
@@ -39,7 +39,7 @@ def both(*args, **kwargs):
 both(5, 6, 7, w=(7, 5), y="harsp", p=False)
 
 =======================
-ef fill_the_box(*args):
+def fill_the_box(*args):
     box = args[0] * args[1] * args[2]
     small_boxes = []
     for s in args[3:]:
@@ -52,3 +52,16 @@ ef fill_the_box(*args):
         return f"There is free space in the box. You could put {box - small_boxes} more cubes."
     else:
         return f"No more free space! You have {small_boxes - box} more cubes."
+
+
+print(fill_the_box(2, 8,
+2, 2, 1, 7, 3, 1, 5,
+"Finish"))
+
+# print(fill_the_box(5, 5,
+# 2, 40, 11, 7, 3, 1, 5,
+# "Finish"))
+
+# print(fill_the_box(10, 10,
+# 10, 40, "Finish", 2, 15,
+# 30))
